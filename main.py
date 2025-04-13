f67bbf3e37b173e4c88ee50edc6f369156143017
zainub-nazish <160176214+zainub-nazish@users.noreply.github.com>
2025-04-13 09:04:17 +0500

    Add files via upload
    Assignment 1 to 6==9

--- main.py
+++ main.py
@@ -1,52 +1,25 @@
-import pygame
-import time
+import streamlit as st
 
-pygame.init()
+st.set_page_config(page_title="My first website",page_icon=" ",layout="centered")
+st.title("Welcome to the my first python website.")
 
-CANVA_WIDTH = 400
-CANVA_HEIGHT = 400
-CELL_SIDE = 40
-ERASER_SIZE = 20
+st. sidebar.title("Navigation")
+page= st.sidebar.radio("Go to",["Home","About","Contact"])
 
-BLUE = (0,0,225)
-WHITE = (255,255,255)
-PINK = (225,182,193)
-
-screen = pygame.display.set_mode((CANVA_WIDTH,CANVA_HEIGHT))
-pygame.display.set_caption("Enter effect in pygame")
-
-grid = []
-for row in range(0,CANVA_HEIGHT,CELL_SIDE):
-    for col in range(0,CANVA_WIDTH,CELL_SIDE):
-        rect = pygame.Rect(col,row,CELL_SIDE,CELL_SIDE)
-        grid.append(rect)
-
-eraser =  pygame.Rect(200,200,ERASER_SIZE,ERASER_SIZE)
-
-running = True
-while running:
-    screen.fill(WHITE)
+if page == "Home":
+    st.header("Home page")
+    st.write("This is a simple home page buil with python and streamlit")
+    name = st.text_input("What\'s your name?")
+    if name: 
+        st.success(f'Hello {name}! Thanks for visiting.')
     
-    for  rect in grid:
-        pygame.draw.rect(screen, BLUE, rect)
+elif page == "About":
+    st.header("About")
+    st.write("This website is built entirely using python and streamlit in under 15 minutes!")
 
-        mouse_x , mouse_y = pygame.mouse.get_pos()
-        eraser.topleft = (mouse_x, mouse_y)
-
-        new_grid = []
-        for rect in grid:
-            if not eraser.colliderect(rect):
-                new_grid.append(rect)
-        grid = new_grid 
-
-        pygame.draw.rect(screen, PINK, eraser)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        pygame.display.flip()
-        time.sleep(0.05)
-
-pygame.quit()        
-
+elif page == "Contact":
+    st.header("Contact Us")
+    email = st.text_input("Your email")
+    message = st.text_input("Your message.")
+    if st.button("Submit"):
+        st.success("Thank you! We have received your message.")
